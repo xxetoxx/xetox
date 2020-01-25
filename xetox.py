2caa829b5436a047bf9380cda522eb67970d6d86
uk <ktsrkyr@gmail.com>
2020-01-25 15:25:34 +0900

    add data

--- xetox.py
+++ xetox.py
@@ -1,59 +1,192 @@
-#PyTorch
-import torch #PyTorchのモジュール
-import torch.nn as nn #基本的なネットワーク構造
-import torch.nn.functional as F #ネットワークの関数
-import torch.optim as optim #最適化関数
-from torch.autograd import Variable #自動微分
-#基本ライブラリ
-import numpy as np #数値計算
-
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
 
 
-# network definition
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(4, 10)
-        self.fc2 = nn.Linear(10, 8)
-        self.fc3 = nn.Linear(8, 3)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-iris = datasets.load_iris()
-y = np.zeros((len(iris.target), 1 + iris.target.max()), dtype=int)
-y[np.arange(len(iris.target)), iris.target] = 1
-X_train, X_test, y_train, y_test = train_test_split(iris.data, y, test_size=0.25)
-x = Variable(torch.from_numpy(X_train).float(), requires_grad=True)
-y = Variable(torch.from_numpy(y_train).float())
-net = Net()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
-criterion = nn.MSELoss()
-
-for i in range(3000):
-    optimizer.zero_grad()
-    output = net(x)
-    loss = criterion(output, y)
-    loss.backward()
-    optimizer.step()
-
-# test
-outputs = net(Variable(torch.from_numpy(X_test).float()))
-_, predicted = torch.max(outputs.data, 1)
-y_predicted = predicted.numpy()
-y_true = np.argmax(y_test, axis=1)
-accuracy = (int)(100 * np.sum(y_predicted == y_true) / len(y_predicted))
-print('accuracy: {0}%'.format(accuracy))
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
-# utility function to predict for an unknown data
-def predict(X):
-    X = Variable(torch.from_numpy(np.array(X)).float())
-    outputs = net(X)
-    return np.argmax(outputs.data.numpy())
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == '__main__':
+    main()
+
+# #PyTorch
+# import torch #PyTorchのモジュール
+# import torch.nn as nn #基本的なネットワーク構造
+# import torch.nn.functional as F #ネットワークの関数
+# import torch.optim as optim #最適化関数
+# #from torch.autograd import Variable #自動微分
+# #基本ライブラリ
+# import numpy as np #数値計算
+
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+
+# print(torch.__version__)
+# # network definition
+# class Net(nn.Module):
+
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.fc1 = nn.Linear(4, 10)
+#         self.fc2 = nn.Linear(10, 8)
+#         self.fc3 = nn.Linear(8, 3)
+
+#     def forward(self, x):
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+
+# iris = datasets.load_iris()
+# y = np.zeros((len(iris.target), 1 + iris.target.max()), dtype=int)
+# y[np.arange(len(iris.target)), iris.target] = 1
+# X_train, X_test, y_train, y_test = train_test_split(iris.data, y, test_size=0.25)
+# # x = Variable(torch.from_numpy(X_train).float(), requires_grad=True)
+# # y = Variable(torch.from_numpy(y_train).float())
+# x = torch.from_numpy(X_train).float()
+# #x.requires_grad = True
+# y = torch.from_numpy(y_train).float()
+
+# net = Net()
+# optimizer = optim.SGD(net.parameters(), lr=0.01)
+# criterion = nn.MSELoss()
+
+# for i in range(3000):
+#     optimizer.zero_grad()
+#     output = net(x)
+#     loss = criterion(output, y)
+#     loss.backward()
+#     optimizer.step()
+
+# # test
+# outputs = net(torch.from_numpy(X_test).float())
+# _, predicted = torch.max(outputs.data, 1)
+# y_predicted = predicted.numpy()
+# y_true = np.argmax(y_test, axis=1)
+# accuracy = (int)(100 * np.sum(y_predicted == y_true) / len(y_predicted))
+# print('accuracy: {0}%'.format(accuracy))
+
+
+# # utility function to predict for an unknown data
+# def predict(X):
+#     X = Variable(torch.from_numpy(np.array(X)).float())
+#     outputs = net(X)
+#     return np.argmax(outputs.data.numpy())
