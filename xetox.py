91f3f4c2ffd8d0b037309d20c1f158be48e867f0
ukyoukatsura <ktsrkyr@gmail.com>
2020-02-02 22:45:53 +0900

    update

--- xetox.py
+++ xetox.py
@@ -29,8 +29,8 @@ class Net(nn.Module):
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self):
         self.df = pd.read_csv("./data/train_data_utf_8_preprocessing.csv", delimiter=",")
-        self.data_num = len(self.df) / 10
-        #self.data_num = 100
+        #self.data_num = len(self.df)
+        self.data_num = 10000
 
     def __len__(self):
         return self.data_num
@@ -78,14 +78,14 @@ def test(args, model, device, test_loader):
 
 def main():
     parser = argparse.ArgumentParser(description='PyTorch implementation of horse racing prediction')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
+    parser.add_argument('--batch-size', type=int, default=100, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR', help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M', help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=1000, metavar='N', help='how many batches to wait before logging training status')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False, help='For Saving the current Model')
     args = parser.parse_args()
 
