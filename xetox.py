8c283574d9faf354f468a1ea97b3d56cf029697a
ukyoukatsura <ktsrkyr@gmail.com>
2020-02-02 22:46:31 +0900

    update

--- xetox.py
+++ xetox.py
@@ -80,7 +80,7 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch implementation of horse racing prediction')
     parser.add_argument('--batch-size', type=int, default=100, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 14)')
+    parser.add_argument('--epochs', type=int, default=100, metavar='N', help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR', help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M', help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
