4e24611e463227652d849a4bac8d0cdb45c43cc6
xxetoxx <50778092+xxetoxx@users.noreply.github.com>
2020-03-06 14:42:38 +0900

    Add files via upload
    とりあえずスクレイピングだけ

--- scraping.py
+++ scraping.py
@@ -1,1 +1,12 @@
-print("hello world!")
+import urllib3
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.ai-yuma.com/"
+
+r =requests.get(url)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+#print(soup.prettify()) # HTMLをインデントすることができます
+print(soup.title.string) #titleのテキストのみ出力
