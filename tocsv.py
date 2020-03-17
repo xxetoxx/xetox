1bb7dbe10b9dfc08947364b069e5457a7521c952
xxetoxx <50778092+xxetoxx@users.noreply.github.com>
2020-03-17 23:26:39 +0900

    Update tocsv.py

--- tocsv.py
+++ tocsv.py
@@ -1,29 +1,25 @@
 import pandas as pd
 import re
 
-
-#\nなくす
-#with open("aaa2.txt", "w", encoding="utf8") as f:
-
-"""
-line
-20200315 高知10R ファイナルレース　Ｃ２　－６記者選抜
-◎ 6【23.0%】 テレグライド(倉兼育)◯ 1【14.2%】 デュパルクカズマ(岡村卓)▲ 4【10.2%】 プリサイスホウプ(佐原秀)△ 7【10.0%】 ドラゴンチャンプ(★濱尚美)☆ 9【 8.7%】 マロンスライム(西川敏)－ 3【 6.9%】 トウカイバレット(西森将)－10【 6.3%】 メメント(☆塚本雄)－12【 5.9%】 コパノハミルトン(大澤誠)－ 2【 5.4%】 マエムキ(木村直)－11【 4.9%】 フユハナビ(林謙佑)－ 8【 2.8%】 クラフツマンシップ(妹尾浩)－ 5【 1.1%】 ハニーフェイバー(☆多田誠)
-"""
-
+#with open("yuma_resultdelete.txt","w",encoding="utf8") as f:
 for line in open("aaa.txt","r",encoding="utf8"):
-    if line[0] != "\n":
-        #ここから考える
-       # print(re.sub(r'.+中央の結果|.+([0-9],[0-9],[0-9]).+',"",line)) #中央の結果記事を削除する
-       # print(re.search(r'[0-9]{8}',line)) #日付
-        #print(re.search(r'[0-9]{,2}R',line)) #レース番号
-        #print(re.search(r'((?<=\d{8}\s).*(?=\d{2}R)|(?<=\d{8}\s).*(?=\d{1}R))',line)) #レース場
-        #print(re.findar'(\d+)【',linell()) #馬番
-        #print(re.findall(r'(\d+\.\d+)%】',line)) #勝率
+     #f.write(re.sub(r'.+中央の結果|.+([0-9],[0-9],[0-9]).+',"",line)) #中央の結果記事を削除する
+     #print(re.search(r'[0-9]{8}',line)) #日付
+     #print(re.search(r'\d+R',line)) #レース番号
+     #print(re.search(r'((?<=\d{8}\s).*(?=\d{2}R)|(?<=\d{8}\s).*(?=\d{1}R))',line)) #レース場
+     #print(re.findall(r'(\d+)【',line)) #馬番
+     #print(re.findall(r'(\d+\.\d+)%】',line)) #勝率
+
 
 # with open("aaa.txt","r",encoding="utf8") as f:
 #     text=f.read()
 # print(text)
 
 
-
+'''
+#delete_\n
+with open("yuma_resultdeletev2.txt","w",encoding="utf8") as f:
+     for line in open("yuma_resultdelete.txt","r",encoding="utf8"):
+         if line[0] != "\n":
+               f.write(line)
+'''
